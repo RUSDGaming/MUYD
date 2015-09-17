547ca879f1b1039a1bdcbb9c3f01e0319960c37c
Shane Dublin <rushanedublin@gmail.com>
2015-09-16 23:24:32 -0700

    set up unity project

---
+++ Assets/Player/CameraFollow.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+
+	// Use this for initialization
+	public Transform playerTransform;
+	public float maxDeltaX = 5;
+	public float maxDeltaZ = 5;
+	private float springyNess = 0f;
+	Transform camTransform;
+	// Use this for initialization
+	void Start ()
+	{
+		camTransform = Camera.main.transform;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+	{
+		
+
+		float deltaX = camTransform.position.x - playerTransform.position.x;
+		float deltaZ = camTransform.position.z - playerTransform.position.z;
+		
+		float x = camTransform.position.x;
+		float z = camTransform.position.z;
+		
+		if (Mathf.Abs (deltaX) > maxDeltaX) {
+			//	Debug.Log ("deltaX: " + deltaX);
+			
+			x = playerTransform.position.x + maxDeltaX * Mathf.Sign (deltaX);
+			//Debug.Log (x);
+		}
+		
+		if (Mathf.Abs (deltaZ) > maxDeltaZ) {
+			z = playerTransform.position.z + maxDeltaZ * Mathf.Sign (deltaZ);
+		}
+		
+		
+		
+		camTransform.position = new Vector3 (x, camTransform.position.y, z);
+		//Debug.Log ("pos:" + transform.position);
+	}
+
+
+}
---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+	// Use this for initialization
+
+
+	public float speed = 6.0F;
+	public float jumpSpeed = 20.0F;
+	public float gravity = 20.0F;
+	private Vector3 moveDirection = Vector3.zero;
+
+	private float horizontal;
+	private float vertical;
+	private bool jump;
+	private CharacterController controller;
+
+
+	void Start ()
+	{
+		controller = GetComponent<CharacterController> ();
+	}
+
+
+	void Update ()
+	{
+		//if (controller.isGrounded) {
+		Debug.Log ("update");
+		if (Input.GetButton ("Jump")) {
+			//Debug.Log ("got the jump button");
+			jump = true;
+		} else {
+			jump = false;
+		}
+		horizontal = Input.GetAxis ("Horizontal");
+		vertical = Input.GetAxis ("Vertical");
+		//Debug.Log ("Horizontal" + horizontal);
+		//}
+	}
+
+
+	void FixedUpdate ()
+	{
+
+	
+		float tempY = moveDirection.y;
+		moveDirection = new Vector3 (horizontal, 0, vertical);
+		moveDirection = transform.TransformDirection (moveDirection);
+		moveDirection *= speed;
+		if (jump && controller.isGrounded) {
+			Debug.Log ("Jumped");
+			moveDirection.y = jumpSpeed;
+			jump = false;
+		} else {
+			moveDirection.y = tempY;
+		}
+		if (jump) {
+			moveDirection.y -= gravity / 2f * Time.fixedDeltaTime;
+		} else {
+			moveDirection.y -= gravity * Time.fixedDeltaTime;
+		}
+		controller.Move (moveDirection * Time.fixedDeltaTime);
+		if (controller.isGrounded) {
+			moveDirection.y = 0;
+		}
+	}
+}
